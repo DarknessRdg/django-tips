1f435a3810ecdce7d704cce3f798135bc04ae761
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-07-19 23:45:22 -0300

    feat: add view de usuarios

--- exemplo/app/views.py
+++ exemplo/app/views.py
@@ -1,5 +1,13 @@
 from rest_framework import viewsets
+from django.contrib.auth.models import User
 from . import models, serializers
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+    authentication_classes = [JWTAuthentication]
 
 
 class ModelComImagemView(viewsets.ModelViewSet):
