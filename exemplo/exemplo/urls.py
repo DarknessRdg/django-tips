3b919956b385ff182793723a368ee8f08c415cf7
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-07-19 23:46:54 -0300

    feat: add url de tokens

--- exemplo/exemplo/urls.py
+++ exemplo/exemplo/urls.py
@@ -15,11 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf.urls.static import static
 from django.conf import settings
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls'))
+    path('', include('app.urls')),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='verify_token'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
