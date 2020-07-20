b09fdf1a1d44e00da74148ed22f45b5761a32a2f
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-07-19 23:44:29 -0300

    test: add tests de token de refesh customizado

--- exemplo/app/tests/test_token.py
+++ exemplo/app/tests/test_token.py
@@ -2,19 +2,14 @@ from rest_framework_simplejwt.utils import datetime_from_epoch
 
 from .api_test_case import APITestCase
 from django.urls import reverse
-from rest_framework_simplejwt import settings
-from app.tokens import TokenDoisDiasAccess, TokenDoisDiasSerializer
+from app.tokens import (
+    TokenDoisDiasAccess, TokenDoisDiasSerializer,
+    TokenTresDiasRefresh, TokenAccessRefreshSerializer
+)
 import datetime
 
 
-class TestTokenWithDifferentDuration(APITestCase):
-    @property
-    def data(self):
-        return {
-            'username': self.username,
-            'password': self.password
-        }
-
+class TestTokenAccess(APITestCase):
     def test_login(self):
         endpoint = reverse('login')
         response = self.client.post(endpoint, self.data)
@@ -41,5 +36,32 @@ class TestTokenWithDifferentDuration(APITestCase):
         token_duration = datetime_from_epoch(token['exp'])
         today = datetime.datetime.now()
 
-        limit_day = today.day + 3
+        limit_day = today.day + 2 + 1
+        self.assertEqual(limit_day, token_duration.day)
+
+
+class TestTokenRefresh(APITestCase):
+    def test_token_serializer(self):
+        serializer = TokenAccessRefreshSerializer(data=self.data)
+        self.assertTrue(serializer.is_valid())
+
+        self.assertIn('access', serializer.validated_data)
+        self.assertIn('refresh', serializer.validated_data)
+
+    def test_pass_view_authentication(self):
+        serializer = TokenAccessRefreshSerializer(data=self.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data['access']
+
+        auth_endpoint = reverse('user-list')
+        response = self.client.get(auth_endpoint, HTTP_AUTHORIZATION=f'JWT {token}')
+        self.assertSuccess(response)
+
+    def test_time(self):
+        token = TokenTresDiasRefresh.for_user(self.user)
+
+        token_duration = datetime_from_epoch(token['exp'])
+        today = datetime.datetime.now()
+
+        limit_day = today.day + 3 + 1
         self.assertEqual(limit_day, token_duration.day)
