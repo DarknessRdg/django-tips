6c9cc6a017ca7cb65971bc790f999b0f65eb0723
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-07-19 23:44:10 -0300

    feat: add tokens de refreh customizados

--- exemplo/app/tokens.py
+++ exemplo/app/tokens.py
@@ -1,5 +1,5 @@
-from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework_simplejwt.serializers import TokenObtainSerializer
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainSerializer, TokenObtainPairSerializer
 from datetime import timedelta
 
 
@@ -18,3 +18,44 @@ class TokenDoisDiasSerializer(TokenObtainSerializer):
         access = self.get_token(self.user)
         data['access'] = str(access)
         return data
+
+
+class TokenTresDiasRefresh(RefreshToken):
+    """
+    Token de refresh com 3 dias de duração, com um token de acesso
+    com 2 dias de duração.
+    """
+    lifetime = timedelta(days=3)
+
+    @property
+    def access_token(self):
+        """
+        Retorna um token de acesso com 2 dias de duração
+        """
+        access = TokenDoisDiasAccess()
+
+        # Use instantiation time of refresh token as relative timestamp for
+        # access token "exp" claim.  This ensures that both a refresh and
+        # access token expire relative to the same time if they are created as
+        # a pair.
+        access.set_exp(from_time=self.current_time)
+
+        no_copy = self.no_copy_claims
+        for claim, value in self.payload.items():
+            if claim in no_copy:
+                continue
+            access[claim] = value
+
+        return access
+
+
+class TokenAccessRefreshSerializer(TokenObtainPairSerializer):
+    """
+    Serializer para criar tokens de `access` e `refresh`.
+    """
+    def get_token(cls, user):
+        """
+        Sobrecreve a criação padrão to token de refresh.
+        """
+        return TokenTresDiasRefresh.for_user(user)
+
